627cbe321dcafea1cf0630a376be9e9a92da19f1
GCHANTRE <78544473+PREDWEEM@users.noreply.github.com>
2025-12-28 13:50:14 -0300

    Update app_emergencia.py

--- app_emergencia.py
+++ app_emergencia.py
@@ -87,7 +87,7 @@ st.sidebar.header("⚙️ Ajustes de Control")
 
 umbral_rel_input = st.sidebar.slider(
     "Sensibilidad de detección", 
-    0.05, 1.00, 0.40, 0.10,
+    0.05, 1.00, 0.50, 0.10,
     help="Define el tamaño del pulso de emergencia necesario para considerar un evento de nacimiento."
 )
 
