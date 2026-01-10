33a3c9e466417eae0b7bfdc830ba26b7587518bc
GCHANTRE <78544473+PREDWEEM@users.noreply.github.com>
2026-01-10 11:58:14 -0300

    Update app_emergencia.py

--- app_emergencia.py
+++ app_emergencia.py
@@ -16,6 +16,7 @@ st.sidebar.image(LOGO_URL, use_container_width=True)
 
 st.set_page_config(
     LOGO_URL = "https://raw.githubusercontent.com/PREDWEEM/loliumTA_2026/main/logo.png"
+    st.set_page.image(LOGO_URL, use_container_width=True)
     page_title="PREDWEEM – LOLIUM TRES ARROYOS 2026", 
     layout="wide",
     page_icon="🌾"
