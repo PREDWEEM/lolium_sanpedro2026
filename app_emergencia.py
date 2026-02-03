a6b7a52f98169e8eee70847f55ce5c9d38c55782
GCHANTRE <78544473+PREDWEEM@users.noreply.github.com>
2026-02-03 16:37:13 -0300

    Update app_emergencia.py

--- app_emergencia.py
+++ app_emergencia.py
@@ -234,7 +234,7 @@ if df is not None and modelo_ann is not None:
     # -----------------------------------------------------
     # VISUALIZACIÓN
     # -----------------------------------------------------
-    st.title("🌾 PREDWEEM LOLIUM-BORDENAVE 2026")
+    st.title("🌾 PREDWEEM LOLIUM- TRES ARROYOS 2026")
 
     # --- GRÁFICO DE CALOR (HEATMAP) ---
     colorscale_hard = [[0.0, "green"], [0.24, "green"], [0.25, "yellow"], [0.74, "yellow"], [0.75, "red"], [1.0, "red"]]
