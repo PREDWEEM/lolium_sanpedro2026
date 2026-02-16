6118eb21b38fe133fb9af10870f3829184849d0c
GCHANTRE <78544473+PREDWEEM@users.noreply.github.com>
2026-02-16 12:18:18 -0300

    Update app_emergencia.py

--- app_emergencia.py
+++ app_emergencia.py
@@ -224,7 +224,7 @@ if df is not None and modelo_ann is not None:
     X = df[["Julian_days", "TMAX", "TMIN", "Prec"]].to_numpy(float)
     emerrel, _ = modelo_ann.predict(X)
     df["EMERREL"] = np.maximum(emerrel, 0.0)
-    df.loc[df["Julian_days"] <= 31, "EMERREL"] = 0.0 
+    df.loc[df["Julian_days"] <= 25, "EMERREL"] = 0.0 
     
     # C. CÁLCULO BIO-TÉRMICO
     df["Tmedia"] = (df["TMAX"] + df["TMIN"]) / 2
