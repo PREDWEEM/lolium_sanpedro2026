84b782017428b4e096c7ea2a41bdfe099d14123d
GCHANTRE <78544473+PREDWEEM@users.noreply.github.com>
2026-03-19 17:32:48 -0300

    Update app_emergencia.py

--- app_emergencia.py
+++ app_emergencia.py
@@ -244,7 +244,7 @@ dga_critico = st.sidebar.number_input("Límite Ventana", value=800, step=50)
 
 st.sidebar.divider()
 st.sidebar.markdown("## 💧 3. Balance Hídrico (Suelo)")
-w_max_val = st.sidebar.number_input("Cap. de Campo Superficial (mm)", value=20.0, step=1.0)
+w_max_val = st.sidebar.number_input("Cap. de Campo Superficial (mm)", value=35.0, step=1.0)
 
 st.sidebar.markdown("**Manejo del Lote (Cobertura)**")
 tipo_manejo = st.sidebar.selectbox(
