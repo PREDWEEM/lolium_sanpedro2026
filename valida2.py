0bf34161da3b39a5d9b1a8e1ccc54992d5d8b8f5
GCHANTRE <78544473+PREDWEEM@users.noreply.github.com>
2026-03-20 13:25:41 -0300

    Update valida2.py

--- valida2.py
+++ valida2.py
@@ -466,7 +466,7 @@ with col_p2:
 
 st.sidebar.divider()
 st.sidebar.markdown("## 💧 4. Balance Hídrico (Suelo)")
-w_max_val = st.sidebar.number_input("Cap. de Campo Superficial (mm)", value=10.0, step=1.0)
+w_max_val = st.sidebar.number_input("Cap. de Campo Superficial (mm)", value=20.0, step=1.0)
 
 st.sidebar.markdown("**Manejo del Lote (Cobertura)**")
 tipo_manejo = st.sidebar.selectbox(
