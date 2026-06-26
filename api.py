4d2f16b996818036609a8ee027f7373d408e957b
GCHANTRE <78544473+PREDWEEM@users.noreply.github.com>
2026-06-26 14:59:13 -0300

    Update api.py

--- api.py
+++ api.py
@@ -1,33 +1,9 @@
-
-from fastapi import FastAPI, UploadFile
-import pandas as pd
-
+from fastapi import FastAPI
 from predweem_core import ejecutar_predweem
 
-app = FastAPI(
-    title="PREDWEEM API"
-)
-
-@app.get("/")
-def root():
-    return {
-        "modelo": "PREDWEEM",
-        "estado": "OK"
-    }
+app = FastAPI()
 
 @app.post("/simular")
-async def simular(
-    clima: UploadFile,
-    campo: UploadFile
-):
-
-    df_clima = pd.read_excel(clima.file)
-    df_campo = pd.read_excel(campo.file)
-
-    resultado = ejecutar_predweem(
-        df_clima,
-        df_campo,
-        {}
-    )
-
+def simular():
+    resultado = ejecutar_predweem(...)
     return resultado
