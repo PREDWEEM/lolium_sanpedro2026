f706fd6288af6030e901b5e50be2f0bc734a5243
GCHANTRE <78544473+PREDWEEM@users.noreply.github.com>
2026-06-26 15:19:00 -0300

    Delete api.py

--- api.py
+++
@@ -1,9 +0,0 @@
-from fastapi import FastAPI
-from predweem_core import ejecutar_predweem
-
-app = FastAPI()
-
-@app.post("/simular")
-def simular():
-    resultado = ejecutar_predweem(...)
-    return resultado
