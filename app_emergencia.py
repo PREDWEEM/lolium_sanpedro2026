dbe9fa9aa5f3e8b4e907a0923341ddd7d063a4e5
GCHANTRE <78544473+PREDWEEM@users.noreply.github.com>
2026-07-08 12:44:58 -0300

    Update app_emergencia.py

--- app_emergencia.py
+++ app_emergencia.py
@@ -509,7 +509,7 @@ dga_critico = st.sidebar.number_input("Límite Ventana (°Cd)", value=800, step=
 
 st.sidebar.divider()
 st.sidebar.markdown("## 💧 3. Balance Hídrico (Suelo)")
-w_max_val = st.sidebar.number_input("Cap. de Campo Superficial (mm)", value=20.0, step=1.0)
+w_max_val = st.sidebar.number_input("Cap. de Campo Superficial (mm)", value=30.0, step=1.0)
 
 st.sidebar.divider()
 st.sidebar.markdown("## 📊 4. Estado de Validación")
