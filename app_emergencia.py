c2cde2edb90ed276bc115fde9b251bdf09666670
GCHANTRE <78544473+PREDWEEM@users.noreply.github.com>
2026-07-14 10:54:40 -0300

    Update app_emergencia.py

--- app_emergencia.py
+++ app_emergencia.py
@@ -447,7 +447,7 @@ with st.expander("📂 1. Datos del Lote", expanded=True):
             st.markdown("#### 🌾 Manejo de Superficie")
             cobertura_pct = st.slider(
                 "Cobertura de Rastrojo en Suelo (%)",
-                min_value=0, max_value=100, value=25, step=5,
+                min_value=0, max_value=100, value=10, step=5,
                 help="0% = Suelo desnudo / Labranza. 100% = Cobertura total (Ej. Cultivo de Servicio)."
             )
 
